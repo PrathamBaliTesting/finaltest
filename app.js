8f56a57f8d1501d03e438f9df1aa12cfeb3876ba
Pratham Baliyan <104058723+PrathamBaliyan@users.noreply.github.com>
2024-08-01 13:22:16 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to My React App</h1>
+            <p>This is a paragraph of text in my React app.</p>
+            <ul>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+            </ul>
+            <a href="https://www.example.com" target="_blank" rel="noopener noreferrer">Visit Example.com</a>
+        </div>
+    );
+}
+
+export default App;
